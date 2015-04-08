eab51a2ee0d184103dcb203197520dd5139f144c
abhilashsn <abhilashsn83@gmail.com>
2015-04-08 08:33:08 -0700

    devise and user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,11 +5,10 @@ class PostsController < ApplicationController
 	before_action :find_post, only:[:show, :edit, :update, :destroy] 
 
 def index
-@posts = Post.all
+@posts = Post.all.order("created_at DESC")
 end
 
 def show 
-
 end
 
 
