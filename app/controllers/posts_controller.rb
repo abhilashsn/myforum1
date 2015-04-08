a2b71885be4c5f2d16e40393767589eb7865f91a
abhilashsn <abhilashsn83@gmail.com>
2015-04-08 02:45:32 -0700

    add edit and all crud

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,61 @@
 class PostsController < ApplicationController
 
+
+
+	before_action :find_post, only:[:show, :edit, :update, :destroy] 
+
 def index
+@posts = Post.all
 end
+
+def show 
+
+end
+
+
+
+def new 
+@posts = Post.new
+end
+
+def create 
+@posts = Post.new(post_params)
+if @posts.save
+	redirect_to @posts
+else
+	render 'new'
+end
+
+end
+
+def edit
+end
+
+
+def destroy
+@posts.destroy
+redirect_to root_path
+end
+
+def update
+	if @posts.update(post_params)
+		redirect_to @posts
+		else
+		render 'edit' 
+end
+end
+
+
+private 
+
+ def find_post
+ 	@posts = Post.find(params[:id])
+end
+
+private 
+
+def post_params
+params.require(:post).permit(:title, :content)
+end
+
 end
